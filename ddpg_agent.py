56efc42a57414b9d8ee5ebb3a90516974d234b44
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2019-01-07 15:23:01 -0500

    feat: add a simple NN as Actor to implement Agent.act()

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -1,18 +1,36 @@
 import numpy as np
+from collections import namedtuple, deque
+from model import Actor
+import torch
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent:
     def __init__(self, num_agents, state_size, action_size):
+        """Initialize an Agent object.
+
+        Params
+        ======
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+        """
         self.num_agents = num_agents
         self.state_size = state_size
         self.action_size = action_size
+        # Actor Network
+        self.actor_local = Actor(state_size, action_size).to(device)
 
     def reset(self):
         pass
 
     def act(self, state):
-        action = np.random.randn(self.num_agents, self.action_size)
-        action = np.clip(action, -1, 1)
-        return action
+        """Returns actions for given state as per current policy."""
+        state = torch.from_numpy(state).float().to(device)
+        self.actor_local.eval()
+        with torch.no_grad():
+            action = self.actor_local(state).cpu().data.numpy()
+        self.actor_local.train()
+        return np.clip(action, -1, 1)
 
     def step(self, state, action, reward, next_state, done):
         pass
