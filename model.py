58411664c05ac97d27f0ddfd5ccdf7f3f3d593ff
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2019-01-07 19:04:05 -0500

    feat: add random seed

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, fc_units=256):
+    def __init__(self, state_size, action_size, seed, fc_units=256):
         """Initialize parameters and build model.
         Params
         ======
@@ -22,6 +22,7 @@ class Actor(nn.Module):
             fc2_units (int): Number of nodes in second hidden layer
         """
         super(Actor, self).__init__()
+        self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc_units)
         self.fc2 = nn.Linear(fc_units, action_size)
         self.reset_parameters()
@@ -38,7 +39,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, fc_units=256):
+    def __init__(self, state_size, action_size, seed, fc_units=256):
         """Initialize parameters and build model.
         Params
         ======
@@ -48,6 +49,7 @@ class Critic(nn.Module):
             fc2_units (int): Number of nodes in second hidden layer
         """
         super(Critic, self).__init__()
+        self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size + action_size, fc_units)
         self.fc2 = nn.Linear(fc_units, 1)
         self.reset_parameters()
