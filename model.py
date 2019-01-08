db9543dcbd4e655701de10a9af01d66c68fbae32
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2019-01-07 21:41:18 -0500

    feat: add a more complex architecture to Critic

--- model.py
+++ model.py
@@ -39,7 +39,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc_units=256):
+    def __init__(self, state_size, action_size, seed, fc1_units=256, fc2_units=256):
         """Initialize parameters and build model.
         Params
         ======
@@ -50,16 +50,19 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size + action_size, fc_units)
-        self.fc2 = nn.Linear(fc_units, 1)
+        self.fc1 = nn.Linear(state_size + action_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
         self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         x = torch.cat((state, action), dim=1)
         x = F.relu(self.fc1(x))
-        return F.tanh(self.fc2(x))
+        x = F.relu(self.fc2(x))
+        return F.tanh(self.fc3(x))
