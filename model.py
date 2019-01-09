0a40ddbf45162a3ad3f6b78caf62f94cd8993bcf
Jaime Andres Castillo Leon <jacastillol@unal.edu.co>
2019-01-09 07:50:54 -0500

    fix: Critic output is not a tanh is just linear

--- model.py
+++ model.py
@@ -65,4 +65,5 @@ class Critic(nn.Module):
         x = torch.cat((state, action), dim=1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        return F.tanh(self.fc3(x))
+        x = self.fc3(x)
+        return x
